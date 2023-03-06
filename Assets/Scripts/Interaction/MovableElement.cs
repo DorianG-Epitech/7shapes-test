a6554821d292f3e4d7e16454a3e9ef43f36da61b
Dorian Gomes <dorian.gomes@epitech.eu>
2023-03-06 11:45:47 +0100

    Prevent post overlapping

--- Assets/Scripts/Interaction/MovableElement.cs
+++ Assets/Scripts/Interaction/MovableElement.cs
@@ -1,25 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 
 // Simple draggable interaction, while the mouse button is pressed, updates the root position of this object with the one obtained by raycasting against the floor
 public class MovableElement : InteractableElement
 {
+    [SerializeField] Collider m_collider;
+    [SerializeField] private List<Collider> m_ignoreColliders;
+    Vector3 m_oldInteractablePosition;
+    Quaternion m_oldInteractableRotation;
+
+    private void Start()
+    {
+        m_collider = GetComponent<Collider>();
+    }
+
     public override void OnInteractableMouseDown(PlayerController controller)
     {
-        // do nothing
+        m_oldInteractablePosition = m_root.transform.position;
+        m_oldInteractableRotation = m_root.transform.rotation;
     }
 
     public override void OnInteractableMouse(PlayerController controller)
     {
         RaycastHit hitFloor;
+
         if (Physics.Raycast(controller.mouseRay, out hitFloor, float.MaxValue, GlobalOptions.main.FloorLayer))
         {
-
+            if (CheckForOverlap(hitFloor.point))
+                return;
             m_root.transform.position = hitFloor.point;
-
-            //@TODO: implement rotation
         }
     }
 
@@ -30,7 +45,31 @@ public class MovableElement : InteractableElement
     
     public override void OnInteractableMouseScroll(PlayerController controller, float scrollDelta)
     {
-        // do nothing
         m_root.transform.Rotate(Vector3.up * scrollDelta);
+    }
+
+    public bool CheckForOverlap(Vector3 newPosition)
+    {
+        Vector3 newPositionOffset = newPosition - m_root.transform.position;
+        Collider[] colliders = Physics.OverlapBox(
+            m_collider.bounds.center + newPositionOffset,
+            m_collider.bounds.extents,
+            m_root.transform.rotation,
+            GlobalOptions.main.ObstacleLayer
+        );
+        foreach (var collider in colliders)
+        {
+            if (m_ignoreColliders.Contains(collider))
+                continue;
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (m_collider != null)
+            Gizmos.DrawWireCube(m_collider.bounds.center, m_collider.bounds.extents * 2);
     }
 }
